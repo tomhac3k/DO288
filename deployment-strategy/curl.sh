9287b78f57c4db61925ab948937171df45c8b883
Mohammed Tahmeed <mdtahmeed@oracle9.3.example.com>
2024-10-11 20:35:19 +0530

    Adding a condition to wait 10 seconds in curl script

--- deployment-strategy/curl.sh
+++ deployment-strategy/curl.sh
@@ -5,6 +5,16 @@ if curl http://localhost:8080 &>/dev/null
 then
 	echo "Web server is active"
 else
-	echo "Web server not found"
+	echo "Waiting for webserver for 10 seconds"
 
+fi
+
+sleep 10;
+
+if curl http://localhost:8080 &>/dev/null
+
+then
+	echo "Webserver is active"
+else
+	echo "Webserver is not comming up"
 fi
